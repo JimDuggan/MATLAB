b401285150c8faab473601e1e955d5999cdd5058
jimduggan <jim.duggan@nuigalway.ie>
2018-02-19 19:12:12 +0000

    Phone market examples... from lecture

--- Code/06 Week/Phones.m
+++ Code/06 Week/Phones.m
@@ -1,11 +1,11 @@
 % based on http://www.math.harvard.edu/~knill/teaching/math19b_2011/handouts/lecture27.pdf
 clear;
 
-C = [0 100]';
+C = [100 0]';
 
-A = [2/3 1/2; 1/3 1/2];
+A = [2/3 0; 1/3 1];
 
-N = 10;
+N = 100;
 out = zeros(N+1,3);
 out(:,1) = 0:N;
 out(1,2:3) = C;
