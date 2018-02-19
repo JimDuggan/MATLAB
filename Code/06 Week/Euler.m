1b594684824cf22b6947f16b1bec857c1c9c3751
jimduggan <jim.duggan@nuigalway.ie>
2018-02-19 13:29:53 +0000

    Update for lectures

---
+++ Code/06 Week/Euler.m
@@ -0,0 +1,27 @@
+% Euler's Method to integrate dy/dt=2t
+% from t=0 to t = 10
+% assume DT = 1 for starters
+clear;
+DT        = 1;
+START   = 0;
+END      = 10;
+
+Time = START;
+y  = 0; % initial value of the integral
+dy_dt = 2 * Time;
+disp(['Time = ' num2str(Time) ' y = ' num2str(y)...
+        ' dy/dy = ' num2str(dy_dt)]); 
+Time = Time + DT;
+
+while Time <= END
+    y = y + dy_dt * DT;
+    dy_dt = 2 * Time;
+    disp(['Time = ' num2str(Time) ' y = ' num2str(y)...
+        ' dy/dy = ' num2str(dy_dt)]); 
+    Time = Time + DT;
+end
+
+
+
+
+
